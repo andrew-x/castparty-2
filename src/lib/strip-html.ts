0b60baa6250b0d1a705cbf435caaf934c0e7a650
Andrew Xia <me@andrewxia.com>
2026-03-30 07:36:58 -0400

    Remove unused strip-html utility

--- src/lib/strip-html.ts
+++
@@ -1,7 +0,0 @@
-import sanitizeHtml from "sanitize-html"
-
-export function stripHtml(html: string): string {
-  return sanitizeHtml(html, { allowedTags: [], allowedAttributes: {} })
-    .replace(/\s+/g, " ")
-    .trim()
-}
